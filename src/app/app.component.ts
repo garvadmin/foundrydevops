dbf1686117484b655ab221472550b22a7044f83b
US\shobhkumar <shobhkumar@deloitte.com>
2019-10-07 18:08:04 +0530

    Adding Updated Package and Template designs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-test';
+export class AppComponent { // implements OnInit when Auth is used
+  title = 'star-admin-angular';
+  // public title = 'My Angular App';
+  // public isAuthenticated: boolean;
+
+  // async ngOnInit() {
+  //   this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  // }
+
+  // constructor(public oktaAuth: OktaAuthService) {
+  //   this.oktaAuth.$authenticationState.subscribe(
+  //     (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
+  //   );
+  // }
+
+  // login() {
+  //   this.oktaAuth.loginRedirect();
+  // }
+
+  // logout() {
+  //   this.oktaAuth.logout('/');
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,113 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule,
+  MatMenuModule,
+  MatProgressSpinnerModule,
+  MatTableModule,
+  MatToolbarModule
+} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { testcomponent } from '../components/testcomponent/testcomponent.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { OktaAuthModule } from '@okta/okta-angular';
+import { ChartComponent } from './chart/chart.component';
+import { ChartModule,
+  AreaSeriesService,
+  StepAreaSeriesService,
+  StackingAreaSeriesService,
+  MultiColoredAreaSeriesService,
+  RangeAreaSeriesService,
+  DateTimeService, BarSeriesService, StackingBarSeriesService } from '@syncfusion/ej2-angular-charts';
+import { CategoryService, ColumnSeriesService } from '@syncfusion/ej2-angular-charts';
+
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsComponent } from './forms/forms.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { TablesComponent } from './tables/tables.component';
+import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { AccordionsComponent } from './accordions/accordions.component';
+import { BadgesComponent } from './badges/badges.component';
+import { ProgressbarComponent } from './progressbar/progressbar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { TooltipsComponent } from './tooltips/tooltips.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    testcomponent
+    testcomponent,
+    ProductsComponent,
+    HomeComponent,
+    ChartComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    DashboardComponent,
+    FormsComponent,
+    ButtonsComponent,
+    TablesComponent,
+    TypographyComponent,
+    IconsComponent,
+    AlertsComponent,
+    AccordionsComponent,
+    BadgesComponent,
+    ProgressbarComponent,
+    BreadcrumbsComponent,
+    PaginationComponent,
+    DropdownComponent,
+    TooltipsComponent,
+    CarouselComponent,
+    TabsComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    ChartModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDividerModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    OktaAuthModule.initAuth({
+      issuer: 'https://dev-989012.okta.com/oauth2/default',
+      redirectUri: 'http://localhost:4200/implicit/callback',
+      clientId: '0oa1gvgskvOqmt7e5357'
+    })
   ],
-  providers: [],
+  providers: [CategoryService,
+    ColumnSeriesService, AreaSeriesService , RangeAreaSeriesService, StepAreaSeriesService, StackingAreaSeriesService,
+    DateTimeService, CategoryService, MultiColoredAreaSeriesService, BarSeriesService, StackingBarSeriesService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
